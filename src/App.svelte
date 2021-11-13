7bc8b2c060030d8b887e7af12e74889bbe7ce940
Missy Danielsen <missy.danielsen@gmail.com>
2021-11-13 13:13:44 -0600

    feat: form with simple submit

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,40 @@
 <script lang="ts">
-	export let name: string;
+	interface FormValues {
+		doseLength?: number;
+		dosesPerDay?: number;
+		startDate?: string;
+		startDoses?: number;
+	}
+
+	let formValues: FormValues = {};
+
+	const onSubmit: svelte.JSX.FormEventHandler<HTMLFormElement> = ({ currentTarget }) => {
+		formValues.doseLength = currentTarget['dose-length'].value;
+		formValues.dosesPerDay = currentTarget['doses-per-day'].value;
+		formValues.startDate = currentTarget['start-date'].value;
+		formValues.startDoses = currentTarget['start-doses'].value;
+
+		console.log(formValues)
+	}
+
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<form on:submit|preventDefault={onSubmit}>
+		<label for="dose-length">Dose Length in Days</label>
+		<input type="number" id="dose-length" min="1" required bind:value={formValues.doseLength}>
+
+		<label for="doses-per-day">Doeses per Day</label>
+		<input type="number" id="doses-per-day" min="1" required bind:value={formValues.dosesPerDay}>
+
+		<label for="start-date">Start Date</label>
+		<input type="date" id="start-date" required bind:value={formValues.startDate}>
+
+		<label for="start-doses">Doses on Start Date</label>
+		<input type="number" id="start-doses" min="1" required bind:value={formValues.startDoses}>
+
+		<button type="reset">Reset</button>
+		<button type="submit">Go</button>
+	</form>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
