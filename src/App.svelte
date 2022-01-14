90e238543a0332f2a2d43eb96e9b7c54d8fb3e3f
Missy Danielsen <missy.danielsen@gmail.com>
2022-01-14 14:21:01 -0600

    refactor: swtich to binding variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,30 @@
 <script lang="ts">
-	interface FormValues {
-		doseLength?: number;
-		dosesPerDay?: number;
-		startDate?: string;
-		startDoses?: number;
+	let doseLength: number;
+	let dosesPerDay: number;
+	let startDate: string;
+	let startDoses: number;
+
+	const onSubmit: svelte.JSX.FormEventHandler<HTMLFormElement> = () => {
+		console.log('length', doseLength)
+		console.log('per day', dosesPerDay)
+		console.log('start', startDate)
+		console.log('start doses', startDoses)
 	}
-
-	let formValues: FormValues = {};
-
-	const onSubmit: svelte.JSX.FormEventHandler<HTMLFormElement> = ({ currentTarget }) => {
-		formValues.doseLength = currentTarget['dose-length'].value;
-		formValues.dosesPerDay = currentTarget['doses-per-day'].value;
-		formValues.startDate = currentTarget['start-date'].value;
-		formValues.startDoses = currentTarget['start-doses'].value;
-
-		console.log(formValues)
-	}
-
-
 </script>
 
 <main>
 	<form on:submit|preventDefault={onSubmit}>
 		<label for="dose-length">Dose Length in Days</label>
-		<input type="number" id="dose-length" min="1" required bind:value={formValues.doseLength}>
+		<input type="number" id="dose-length" min="1" required bind:value={doseLength}>
 
 		<label for="doses-per-day">Doeses per Day</label>
-		<input type="number" id="doses-per-day" min="1" required bind:value={formValues.dosesPerDay}>
+		<input type="number" id="doses-per-day" min="1" required bind:value={dosesPerDay}>
 
 		<label for="start-date">Start Date</label>
-		<input type="date" id="start-date" required bind:value={formValues.startDate}>
+		<input type="date" id="start-date" required bind:value={startDate}>
 
 		<label for="start-doses">Doses on Start Date</label>
-		<input type="number" id="start-doses" min="1" required bind:value={formValues.startDoses}>
+		<input type="number" id="start-doses" min="1" required bind:value={startDoses}>
 
 		<button type="reset">Reset</button>
 		<button type="submit">Go</button>
